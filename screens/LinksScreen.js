b83db4082ef84d6d2c3c9b4712c36224607ba582
kitttipong <ping90750@gmail.com>
2019-08-17 14:03:07 +0700

    test location

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,27 +1,117 @@
-import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
-import { ExpoLinksView } from '@expo/samples';
+import React, { Component } from "react";
+import { ScrollView, StyleSheet, Platform, AsyncStorage } from "react-native";
+import * as Location from "expo-location";
+import * as TaskManager from "expo-task-manager";
+import { ExpoLinksView } from "@expo/samples";
 
-export default function LinksScreen() {
-  return (
-    <ScrollView style={styles.container}>
-      {/**
-       * Go ahead and delete ExpoLinksView and replace it with your content;
-       * we just wanted to provide you with some helpful links.
-       */}
-      <ExpoLinksView />
-    </ScrollView>
-  );
+export default class LinksScreen extends Component<Props> {
+  static navigationOptions = ({ navigation }) => {
+    const params = navigation.state.params || {};
+    return {
+      headerStyle: { backgroundColor: "#F9F9F9" },
+      headerTitle: (
+        <Text
+          style={{
+            flex: 1,
+            fontSize: width * 0.05,
+            fontWeight: "bold",
+            textAlign: "center",
+            color: "#707070"
+          }}
+        >
+          LinksScreen
+        </Text>
+      ),
+      headerLeft: <View style={{ flex: 1 }} />,
+      headerRight: (
+        <TouchableOpacity
+          hitSlop={{
+            top: width * 0.04,
+            bottom: width * 0.04,
+            left: width * 0.1,
+            right: width * 0.1
+          }}
+        >
+          <Text
+            style={{ marginRight: 20, color: "#FA7268", fontWeight: "600" }}
+          >
+            Done
+          </Text>
+        </TouchableOpacity>
+      )
+    };
+  };
+
+  constructor(props) {
+    super(props);
+    const { params } = this.props.navigation.state;
+    this.state = {
+      location: null,
+      errorMessage: null
+    };
+  }
+  async componentWillMount() {
+    if (Platform.OS === "android" && !Constants.isDevice) {
+      this.setState({
+        errorMessage:
+          "Oops, this will not work on Sketch in an Android emulator. Try it on your device!"
+      });
+    } else {
+      this._getLocationAsync();
+    }
+    // await AsyncStorage.setItem(
+    //   "currentUser",
+    //   JSON.stringify(this.props.currentUser)
+    // );
+
+    await Location.startLocationUpdatesAsync("task1", {
+      accuracy: Location.Accuracy.Balanced,
+      timeInterval: 1000,
+      distanceInterval: 0
+    });
+  }
+  // static getCurrentUser = async () => {
+  //   const currentUser = await AsyncStorage.getItem("currentUser");
+  //   return JSON.parse(currentUser);
+  // };
+  _getLocationAsync = async () => {
+    let location = await Location.getCurrentPositionAsync({});
+    this.setState({ location });
+    console.warn("Location", this.state.location);
+  };
+
+  render() {
+    return (
+      <ScrollView style={styles.container}>
+        {/**
+         * Go ahead and delete ExpoLinksView and replace it with your content;
+         * we just wanted to provide you with some helpful links.
+         */}
+        <ExpoLinksView />
+      </ScrollView>
+    );
+  }
 }
-
+TaskManager.defineTask("task1", async ({ data, error }) => {
+  if (error) {
+    // check `error.message` for more details.
+    return;
+  }
+  if (data) {
+    console.log("You've entered region:", data);
+    const { location } = data;
+    const currentUser = await LinksScreen.getCurrentUser();
+    console.log("currentUser", currentUser);
+  }
+});
 LinksScreen.navigationOptions = {
-  title: 'Links',
+  title: "Links"
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: 15,
-    backgroundColor: '#fff',
-  },
+    backgroundColor: "#fff"
+  }
 });
